1dfbf1989f8e34e4a7d8910f60aa04bd49743129
Karina Borges <karina_borges@id.uff.br>
2021-02-21 20:53:45 -0300

    changes

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+
+export default props =>
+  <footer className="footer">
+    <spam>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+      <strong> <span className="text-danger">K</span>arina</strong>
+      <strong> <span className="text-danger">B</span>orges</strong>
+    </spam>
+  </footer>
